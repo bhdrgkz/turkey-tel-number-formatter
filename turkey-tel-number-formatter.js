a51c6da1216989a9294f1d38b2cf6a706b46c233
bhdrgkz <bahadir@gokoz.com>
2015-06-24 18:37:35 +0300

    default empty value added if length zero

--- turkey-tel-number-formatter.js
+++ turkey-tel-number-formatter.js
@@ -1,5 +1,6 @@
 function ttnf(telNumber) {
   "use strict";
+  var newFormat;
   telNumber = telNumber.replace(/\D/g,'');
   telNumber = telNumber.slice(-10);
   output = [],
@@ -7,7 +8,10 @@ function ttnf(telNumber) {
   for (var i = 0, len = sNumber.length; i < len; i += 1) {
       output.push(+sNumber.charAt(i));
   }
-  var newFormat = "0 (" + output[0]+output[1]+output[2] + ") " + output[3]+output[4]+output[5] + " " + output[6]+output[7] + " " + output[8]+output[9];
+  newFormat = "0 (" + output[0]+output[1]+output[2] + ") " + output[3]+output[4]+output[5] + " " + output[6]+output[7] + " " + output[8]+output[9];
+  if(newFormat.length < 17) {
+    newFormat = "";
+  }
   return newFormat;
 }
 module.exports = function (str) {
